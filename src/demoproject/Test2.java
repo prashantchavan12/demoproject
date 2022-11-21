9be61d336e6aa77f53837f3edca1f24830d6223b
prashantchavan5664 <parshuu6689@gmail.com>
2022-11-21 12:47:50 +0530

    commit to branchx

---
+++ src/demoproject/Test2.java
@@ -0,0 +1,20 @@
+package demoproject;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry ="yes";
+		
+		if(hungry=="yes") {
+			
+			System.out.println("eat");
+		}
+		else {
+			System.out.println("work");
+		}
+
+	}
+
+}
